389c52d12def17c479dc90b5c0d58dd9428ef88d
Rachel Colqhoun <rachel.colquhoun@ed.ac.uk>
2020-01-14 16:55:06 +0000

    allow threads/cores to be specified and passed as snakemake command (not a config option)

--- postbox/postbox.py
+++ postbox/postbox.py
@@ -131,7 +131,7 @@ def main():
     sample_dict = update_sample_dict_with_csv(args.csv, sample_dict)
     dict_string = sample_dict_to_dict_string(sample_dict)
 
-    command_list = ['snakemake', '--snakefile', pipeline_dict["path"], "--cores", args.threads]
+    command_list = ['snakemake', '--snakefile', pipeline_dict["path"], "--cores", str(args.threads)]
     if pipeline_dict["config_file"] is not None:
         command_list.extend(["--configfile", pipeline_dict["config_file"]])
     command_list.extend(["--config samples=%s" %dict_string, "basecalled_path=%s" %config["basecalledPath"]])
