5b902708a510e5d9d341bdbe946921a0504100bd
Rachel Colqhoun <rachel.colquhoun@ed.ac.uk>
2020-01-14 16:23:17 +0000

    pass in protocols directory and assume run from run directory

--- postbox/postbox.py
+++ postbox/postbox.py
@@ -1,7 +1,12 @@
-import pandas as pd
+
+
 import subprocess
 import argparse
 import sys
+import os.path
+import json
+import pandas as pd
+
 
 class Error (Exception): pass
 
@@ -11,11 +16,17 @@ def get_arguments():
     '''
     parser = argparse.ArgumentParser(description='Parses CSV output by RAMPART and runs analysis step on all barcoded samples')
 
-    main_group = parser.add_argument_group('Options')
-    main_group.add_argument('-c', '--csv', dest='csv', required=True,
-                            help='CSV file output by RAMPART containing columns for barcodes and samples (required).')
-    main_group.add_argument('-s', '--snakefile', dest='snakefile', default='rampart/pipelines/analyse_all/Snakefile')
-    main_group.add_argument('remainder', nargs=argparse.REMAINDER)
+    main_group = parser.add_argument_group('Main options')
+    main_group.add_argument('-p', '--protocol', dest='protocol', required=True,
+                            help='Path to RAMPART protocol directory')
+    main_group.add_argument('-q', '--pipeline', dest='pipeline', default=None,
+                            help='Name of pipeline to run')
+
+    run_group = parser.add_argument_group('Run configuration options')
+    run_group.add_argument('-r', '--run_configuration', dest='run_configuration', default='./run_configuration.json')
+    run_group.add_argument('-c', '--csv', dest='csv', default='./barcodes.csv')
+
+    run_group.add_argument('remainder', nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
 
@@ -34,7 +45,51 @@ def syscall(command, allow_fail=False):
     print(completed_process.stdout)
     return completed_process
 
-def csv_to_dict_string(csv_file):
+def find_pipeline(protocol_path, pipeline_name, pipeline_dict):
+    pipeline_json = protocol_path + "/rampart/pipelines.json"
+    if not os.path.exists(pipeline_json):
+        sys.exit(
+            'Error: %s does not exist. Does the protocols directory have the correct format?' %pipeline_json)
+
+    snakemake = protocol_path + "/rampart/"
+
+    with open(pipeline_json) as json_file:
+        pipelines = json.load(json_file)
+        assert pipeline_name is not None or len(pipelines) == 1
+        if pipeline_name is None:
+            pipeline_name = list(pipelines.keys())[0]
+
+        assert pipeline_name in pipelines
+        pipeline_dict.update(pipelines[pipeline_name])
+
+        assert "path" in pipelines[pipeline_name]
+        snakemake += pipelines[pipeline_name]["path"] + "/Snakefile"
+        print(snakemake)
+        if not os.path.exists(snakemake):
+            sys.exit(
+                'Error: %s does not exist. Does the protocols directory have the correct format?' % snakemake)
+
+        pipeline_dict["path"] = snakemake
+
+    return pipeline_dict
+
+def load_run_configuration(run_configuration):
+    if not os.path.exists(run_configuration):
+        sys.exit(
+            'Error: %s does not exist. If not in current working directory, please specify the filepath with '
+            '--run_configuration parameter' %run_configuration)
+    with open(run_configuration) as json_file:
+        config = json.load(json_file)
+        print(config)
+
+    sample_dict = {}
+    if "samples" in config:
+        for sample in config["samples"]:
+            sample_dict[sample["name"]] = sample["barcodes"]
+    print(sample_dict)
+    return config, sample_dict
+
+def csv_to_sample_dict(csv_file):
     csv = pd.read_csv(csv_file)
     sample_dict = {}
 
@@ -43,8 +98,15 @@ def csv_to_dict_string(csv_file):
         if sample not in sample_dict:
             sample_dict[sample] = []
         sample_dict[sample].append(barcode)
-
     print(sample_dict)
+    return sample_dict
+
+def update_sample_dict_with_csv(csv_file, sample_dict):
+    if os.path.exists(csv_file):
+        sample_dict = csv_to_sample_dict(csv_file)
+    return sample_dict
+
+def sample_dict_to_dict_string(sample_dict):
     sample_strings = ["%s: [%s]" %(sample, ",".join(sample_dict[sample])) for sample in sample_dict]
     dict_string = "'{%s}'" %", ".join(sample_strings)
     print(dict_string)
@@ -52,12 +114,28 @@ def csv_to_dict_string(csv_file):
 
 def main():
     args = get_arguments()
-    sample_dict = csv_to_dict_string(args.csv)
-    command_list = ['snakemake', '--snakefile', args.snakefile, "--config samples=%s" %sample_dict]
+
+    pipeline_dict = {
+        "path": None,
+        "config": None,
+        "config_file": None,
+        "options": None
+    }
+    pipeline_dict = find_pipeline(args.protocol, args.pipeline, pipeline_dict)
+    print(pipeline_dict)
+
+    config, sample_dict = load_run_configuration(args.run_configuration)
+    sample_dict = update_sample_dict_with_csv(args.csv, sample_dict)
+    dict_string = sample_dict_to_dict_string(sample_dict)
+
+    command_list = ['snakemake', '--snakefile', pipeline_dict["path"]]
+    if pipeline_dict["config_file"] is not None:
+        command_list.extend(["--configfile", pipeline_dict["config_file"]])
+    command_list.extend(["--config samples=%s" %dict_string, "basecalled_path=%s" %config["basecalledPath"]])
     command_list.extend(args.remainder)
     command = ' '.join(command_list)
     print(command)
-    syscall(command)
+    #syscall(command)
 
 if __name__ == '__main__':
     main()
