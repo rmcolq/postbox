d6acfb5359c110789fc3ff95bde91540f02c8243
rmcolq <rmnorris@well.ox.ac.uk>
2020-01-16 14:43:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="postbox",
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     url="https://github.com/rmcolq/postbox",
     license="MIT",
